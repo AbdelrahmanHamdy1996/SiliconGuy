563d0070cfad2c2809e9c3d99c4542e09360a01b
Abdelrahman Ali <hamdyabdelrahman200@gmail.com>
2022-09-17 17:14:03 +0200

    change showcase  price

--- src/components/sections/Showcase.js
+++ src/components/sections/Showcase.js
@@ -161,20 +161,20 @@ const Showcase = () => {
   return(
     <Section id="showcase">
     <Row direction="none" ref={Row1Ref}>
-      <NftItem img={img1}  number={852} price={1}   passRef = {Row1Ref} />
-      <NftItem img={img2}  number={123} price={1.2}   passRef = {Row1Ref} />
-      <NftItem img={img3}  number={456} price={2.5}   passRef = {Row1Ref} />
-      <NftItem img={img4}  number={666} price={3.5}   passRef = {Row1Ref} />
-      <NftItem img={img5}  number={452} price={4.7}   passRef = {Row1Ref} />
+      <NftItem img={img1}  number={852} price={0.1}   passRef = {Row1Ref} />
+      <NftItem img={img2}  number={123} price={0.1}   passRef = {Row1Ref} />
+      <NftItem img={img3}  number={456} price={0.1}   passRef = {Row1Ref} />
+      <NftItem img={img4}  number={666} price={0.1}   passRef = {Row1Ref} />
+      <NftItem img={img5}  number={452} price={0.1}   passRef = {Row1Ref} />
 
 
     </Row>
     <Row direction="reverse" ref={Row2Ref}>
-    <NftItem img={img6}  number={888} price={1.2}   passRef = {Row2Ref} />
-    <NftItem img={img7}  number={211} price={3.2}   passRef = {Row2Ref} />
-    <NftItem img={img8}  number={455} price={1.8}   passRef = {Row2Ref} />
-    <NftItem img={img9}  number={456} price={5.1}   passRef = {Row2Ref} />
-    <NftItem img={img10}  number={865} price={3.7}   passRef = {Row2Ref} />
+    <NftItem img={img6}  number={888} price={0.1}   passRef = {Row2Ref} />
+    <NftItem img={img7}  number={211} price={0.1}   passRef = {Row2Ref} />
+    <NftItem img={img8}  number={455} price={0.1}   passRef = {Row2Ref} />
+    <NftItem img={img9}  number={456} price={0.1}   passRef = {Row2Ref} />
+    <NftItem img={img10}  number={865} price={0.1}   passRef = {Row2Ref} />
 
 
     </Row>
